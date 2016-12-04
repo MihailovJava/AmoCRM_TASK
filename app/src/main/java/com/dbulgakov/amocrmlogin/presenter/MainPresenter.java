7e6568464b666204df5b7d7039cde291f5862800
Dmytro Bulhakov <mail@dbulgakov.com>
2016-12-04 21:10:03 +0300

    implemented get credentials method

--- app/src/main/java/com/dbulgakov/amocrmlogin/presenter/MainPresenter.java
+++ app/src/main/java/com/dbulgakov/amocrmlogin/presenter/MainPresenter.java
@@ -1,6 +1,10 @@
 package com.dbulgakov.amocrmlogin.presenter;
 
+import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.accounts.AccountManagerFuture;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
 
 import com.dbulgakov.amocrmlogin.other.App;
@@ -15,6 +19,9 @@ import rx.Scheduler;
 public class MainPresenter extends BasePresenter {
 
     private final MainView mainView;
+    private String userDomain;
+    private String userEmail;
+    private String userApiKey;
 
     @Inject
     @Named(Const.UI_THREAD)
@@ -37,6 +44,8 @@ public class MainPresenter extends BasePresenter {
     public void checkAuth() {
         if (!hasLoggedAccount()) {
             startLoginActivity();
+        } else {
+            getUserCredentials();
         }
     }
 
@@ -47,5 +56,26 @@ public class MainPresenter extends BasePresenter {
 
     private void startLoginActivity(){
         mainView.startLoginActivity();
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void getUserCredentials() {
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    Account[] accs = accountManager.getAccountsByType(Const.ACCOUNT_TYPE);
+                    Bundle bnd = accountManager.getAuthToken(accs[0], AccountManager.KEY_AUTHTOKEN, null, null, null, null).getResult();
+                    userDomain = accountManager.getUserData(accs[0], Const.USER_ACCOUNT_DOMAIN_KEY);
+                    userApiKey = bnd.getString(AccountManager.KEY_AUTHTOKEN);
+                    userEmail = bnd.getString(AccountManager.KEY_ACCOUNT_NAME);
+                } catch (Exception e) {
+                    Log.e("Error", e.getMessage());
+                }
+
+                return null;
+            }
+        };
+        task.execute();
     }
 }
